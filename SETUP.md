e7e261e27688ba5937c17101ac5ced453c131750
dcmartin <github@dcmartin.com>
2020-04-08 20:54:47 -0700

    lgtm

--- SETUP.md
+++ SETUP.md
@@ -1,6 +1,6 @@
 # `SETUP.md` - Setting up Open Horizon
 
-These instructions are for Debian LINUX, notably Ubuntu and Raspbian.
+These instructions are for Debian LINUX, notably Ubuntu and Raspbian.  Instructions for _macOS_ are TBD.
 
 
 ## Step 1 - Setup _exchange_
@@ -12,7 +12,7 @@ cd ~/GIT/open-horizon
 echo ${USER} > HZN_ORG_ID && export HZN_ORG_ID=$(cat HZN_ORG_ID)
 echo ${USER} > HZN_USER_ID && export HZN_USER_ID=$(cat HZN_USER_ID)
 echo http://exchange:3090/v1/ > HZN_EXCHANGE_URL && export HZN_EXCHANGE_URL=$(cat HZN_EXCHANGE_URL)
-echo whocares > HZN_EXCHANGE_APIKEY&& export HZN_EXCHANGE_APIKEY=$(cat HZN_EXCHANGE_APIKEY)
+echo whocares > HZN_EXCHANGE_APIKEY && export HZN_EXCHANGE_APIKEY=$(cat HZN_EXCHANGE_APIKEY)
 cd exchange/ && ln -s ../HZN* .
 # edit config.json ..
 make up
@@ -20,7 +20,7 @@ make prime
 ```
 
 ## Step 2 - Setup _agent_
-Change directory to top-level of this cloned repository and run the provided script to download the Debian packages for your LINUX _flavor_ and _distribution_, e.g. `ubuntu:bionic` or `raspbian:buster`; for example:
+Change directory to top-level of this cloned repository and run the provided script to download the Debian packages for your LINUX _flavor_ and _distribution_, e.g. `ubuntu:bionic` or `raspbian:buster`. This script will also work under _macOS_; for example:
 
 ```
 cd ~/GIT/open-horizon && sudo ./sh/get.horizon.sh
@@ -43,7 +43,7 @@ export HZN_ORG_ID=${HZN_ORG_ID:-${USER}}
 export HZN_USER_ID=${HZN_USER_ID:-${USER}}
 export HZN_EXCHANGE_APIKEY=${HZN_EXCHANGE_APIKEY:-whocares}
 export HZN_EXCHANGE_USER_AUTH=${HZN_ORG_ID}/${HZN_USER_ID}:${HZN_EXCHANGE_APIKEY}
-hzn node list
+hzn exchange service list
 ```
 
 ## Step 4 - Build
@@ -71,11 +71,15 @@ make publish
 ## Step 7 - Test
 
 ```
+hzn exchange service list
+```
+
+```
 cd ~/GIT/open-horizon/services
 ./sh/lsservices.sh
 ```
 
-## Step 8 - All architectures **(only macOS)**
+## Step 8 - All architectures _(only macOS)_
 
 ```
 cd ~/GIT/open-horizon/services
--- sh/get.horizon.sh
+++ sh/get.horizon.sh
@@ -1,50 +1,130 @@
 #!/bin/bash
 
+##
+install_defaults()
+{
+  local result
+
+  if [ -s HZN_EXCHANGE_URL ]; then HZN_EXCHANGE_URL=$(cat HZN_EXCHANGE_URL); fi
+  if [ "${HZN_EXCHANGE_URL:-null}" != 'null' ]; then
+    echo 'Updating /etc/default/horizon with HZN_EXCHANGE_URL="'${HZN_EXCHANGE_URL}'"' &> /dev/stderr
+    sed -i -e "s|^HZN_EXCHANGE_URL=.*|HZN_EXCHANGE_URL=${HZN_EXCHANGE_URL}|" /etc/default/horizon
+    result=0
+  else
+    echo 'Edit /etc/default/horizon and specify HZN_EXCHANGE_URL; then "sudo systemctl restart horizon"' &> /dev/stderr
+  fi
+
+  if [ -s HZN_FSS_CSSURL ]; then HZN_FSS_CSSURL=$(cat HZN_FSS_CSSURL); fi
+  if [ "${HZN_FSS_CSSURL:-null}" != 'null' ]; then
+    echo 'Updating /etc/default/horizon with HZN_FSS_CSSURL="'${HZN_FSS_CSSURL}'"' &> /dev/stderr
+    sed -i -e "s|^HZN_FSS_CSSURL=.*|HZN_FSS_CSSURL=${HZN_FSS_CSSURL}|" /etc/default/horizon
+    result=0
+  else
+    echo 'Edit /etc/default/horizon and specify HZN_FSS_CSSURL; then "sudo systemctl restart horizon"' &> /dev/stderr
+  fi
+  if [ "${result:-1}" -eq 0 ]; then systemctl restart horizon; fi
+  echo ${result:-1}
+}
+
+##
+install_linux()
+{
+  if [ "${DEBUG:-false}" = 'true' ]; then echo "${FUNCNAME[0} ${*}" &> /dev/stderr; fi
+
+  # which version
+  local version=${1}
+  local result
+
+  # LINUX specifics
+  local arch=$(dpkg --print-architecture)
+  local platform=$(lsb_release -a 2> /dev/null | egrep 'Distributor ID:' | awk '{ print $3 }' | tr '[:upper:]' '[:lower:]')
+  local dist=$(lsb_release -a 2> /dev/null | egrep 'Codename:' | awk '{ print $2 }')
+  local repo=http://pkg.bluehorizon.network/linux
+  local dir=pool/main/h/horizon
+  
+  if [ ! -z "$(command -v hzn)" ]; then
+    echo 'The "hzn" command is already installed; remove with "sudo dpkg --purge bluehorizon horizon horizon-cli"' &> /dev/stderr
+  else
+    # download packages
+    for p in horizon-cli horizon bluehorizon; do
+      if [ ! -s ${p}.deb ]; then
+        if [ "${p}" = 'bluehorizon' ]; then dep=all; else dep=${arch}; fi
+        package=${dir}/${p}
+        curl -sSL ${repo}/${platform}/${package}_${version}~ppa~${platform}.${dist}_${dep}.deb -o ${p}.deb
+      fi
+      dpkg -i ${p}.deb
+    done
+  fi
+  
+  install_defaults
+  
+  echo ${result:-0}
+}
+
+install_darwin()
+{
+  if [ "${DEBUG:-false}" = 'true' ]; then echo "${FUNCNAME[0} ${*}" &> /dev/stderr; fi
+
+  # which version
+  local version=${1}
+  local crt="http://pkg.bluehorizon.network/macos/certs/horizon-cli.crt"
+  local pkg="http://pkg.bluehorizon.network/macos/horizon-cli-${version}.pkg"
+  local result
+
+  if [ ! -s "horizon-cli.crt" ]; then
+    curl -sSL "${crt}" -o horizon-cli.crt
+  fi
+  if [ ! -s "horizon-cli.pkg" ]; then
+    curl -sSL "${pkg}" -o horizon-cli.pkg
+  fi
+
+  if [ -s "horizon-cli.crt" ]; then
+    result=$(security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain horizon-cli.crt)
+    if [ ${result} -eq 0 ]; then
+      if [ -s "horizon-cli.pkg" ]; then
+        result=$(installer -pkg "horizon-cli-2.24.18.pkg" -target /)
+      else
+        echo 'Unable to download package; URL: ${pkg}' &> /dev/stderr
+      fi
+      if [ ${result} -ne 0 ]; then
+        echo 'Unable to install package; result: ${result}' &> /dev/stderr
+      fi
+    else
+      echo 'Unable to add trusted certificate; result: ${result}' &> /dev/stderr
+    fi
+  else
+    echo 'Unable to download certificate; URL: ${crt}' &> /dev/stderr
+  fi
+  echo ${result:-1}
+}
+
+##
+
+get_horizon()
+{
+  local version=${1}
+  local result
+
+  case $(uname) in 
+    Linux)
+      result=$(install_linux ${version})
+      ;;
+    Darwin)
+      result=$(install_darwin ${version})
+      ;;
+  esac
+  echo ${result:-1}
+}
+
+###
+### MAIN
+###
+
+STABLE_VERSION=2.24.18
+
 if [ "${USER:-}" != 'root' ]; then
   echo "Run as root: sudo ${0} ${*}" &> /dev/stderr
   exit 1
-fi
-
-# get particulars
-arch=$(dpkg --print-architecture)
-platform=$(lsb_release -a 2> /dev/null | egrep 'Distributor ID:' | awk '{ print $3 }' | tr '[:upper:]' '[:lower:]')
-dist=$(lsb_release -a 2> /dev/null | egrep 'Codename:' | awk '{ print $2 }')
-
-# only working version
-repo=http://pkg.bluehorizon.network/linux
-dir=pool/main/h/horizon
-version=2.24.18
-
-if [ ! -z "$(command -v hzn)" ]; then
-  echo 'The "hzn" command is already installed; remove with "sudo dpkg --purge bluehorizon horizon horizon-cli"' &> /dev/stderr
 else
-  # download packages
-  for p in horizon-cli horizon bluehorizon; do
-    if [ ! -s ${p}.deb ]; then
-      if [ "${p}" = 'bluehorizon' ]; then dep=all; else dep=${arch}; fi
-      package=${dir}/${p}
-      curl -sSL ${repo}/${platform}/${package}_${version}~ppa~${platform}.${dist}_${dep}.deb -o ${p}.deb
-    fi
-    dpkg -i ${p}.deb
-  done
+  exit $(get_horizon ${1:-${STABLE_VERSION}})
 fi
-
-if [ -s HZN_EXCHANGE_URL ]; then HZN_EXCHANGE_URL=$(cat HZN_EXCHANGE_URL); fi
-if [ "${HZN_EXCHANGE_URL:-null}" != 'null' ]; then
-  echo 'Updating /etc/default/horizon with HZN_EXCHANGE_URL="'${HZN_EXCHANGE_URL}'"' &> /dev/stderr
-  sed -i -e "s|^HZN_EXCHANGE_URL=.*|HZN_EXCHANGE_URL=${HZN_EXCHANGE_URL}|" /etc/default/horizon
-  restart=true
-else
-  echo 'Edit /etc/default/horizon and specify HZN_EXCHANGE_URL; then "sudo systemctl restart horizon"' &> /dev/stderr
-fi
-
-if [ -s HZN_FSS_CSSURL ]; then HZN_FSS_CSSURL=$(cat HZN_FSS_CSSURL); fi
-if [ "${HZN_FSS_CSSURL:-null}" != 'null' ]; then
-  echo 'Updating /etc/default/horizon with HZN_FSS_CSSURL="'${HZN_FSS_CSSURL}'"' &> /dev/stderr
-  sed -i -e "s|^HZN_FSS_CSSURL=.*|HZN_FSS_CSSURL=${HZN_FSS_CSSURL}|" /etc/default/horizon
-  restart=true
-else
-  echo 'Edit /etc/default/horizon and specify HZN_FSS_CSSURL; then "sudo systemctl restart horizon"' &> /dev/stderr
-fi
-
-if [ "${restart:-false}" = 'true' ]; then systemctl restart horizon; fi
