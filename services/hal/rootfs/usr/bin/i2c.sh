a1698b0e7ecb01fba39541de72b69bb860730893
dcmartin <github@dcmartin.com>
2020-05-02 00:08:50 +0100

    lgtm

--- services/hal/rootfs/usr/bin/i2c.sh
+++ services/hal/rootfs/usr/bin/i2c.sh
@@ -4,7 +4,7 @@ if [ -z $(command -v "i2cdetect") ]; then
   exit 1
 fi
 echo -n '{"i2c":'
-i2cdetect -y 1 2> /dev/null | sed 's/^\(..\)[^:]/key/' | sed 's/   / xx/g' | sed 's/ [ ]*/,/g' | sed 's/,$//' | csvjson
+i2cdetect -y 1 2> /dev/null | sed 's/^..[^:]/key:/' | sed 's/\([^:]*\):/\1/' | sed 's/   / xx/g' | sed 's/ [ ]*/,/g' | sed 's/,$//' | csvjson
 echo '}'
 exit 0
 
