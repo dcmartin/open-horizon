cbe2f348ca30dfa26f5131c0d57312efe9a4685f
dcmartin <github@dcmartin.com>
2020-06-07 11:58:29 -0700

    lgtm

--- services/yolo/rootfs/usr/bin/yolo-tools.sh
+++ services/yolo/rootfs/usr/bin/yolo-tools.sh
@@ -351,7 +351,7 @@ yolo_annotate()
 
   local json=${1}
   local jpeg=${2}
-  local colors=(green yellow orange magenta blue cyan lime gold pink white)
+  local colors=(lime greenyellow yellow gold goldenrod orange darkorange coral orangered crimson red)
   local rtg=(FF0000 FF1100 FF2300 FF3400 FF4600 FF5700 FF6900 FF7B00 FF8C00 FF9E00 FFAF00 FFC100 FFD300 FFE400 FFF600 F7FF00 E5FF00 D4FF00 C2FF00 B0FF00 9FFF00 8DFF00 7CFF00 6AFF00 58FF00 47FF00 35FF00 24FF00 12FF00 00FF00)
   local nrtg=${#rtg[@]}
 
