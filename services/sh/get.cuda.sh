f21c78b987ef4b4c46e7f324ce6f9904147fc8ea
dcmartin <github@dcmartin.com>
2020-06-08 18:11:44 -0700

    lgtm

--- services/sh/get.cuda.sh
+++ services/sh/get.cuda.sh
@@ -5,4 +5,4 @@ sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt update -qq -y
-sudo apt install -qq -y install cuda nvidia-cuda-toolkit cuda-drivers    
+sudo apt install -qq -y install cuda nvidia-cuda-toolkit
