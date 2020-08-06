aa8ead90f9a0cffe0b7465bbb1bd445f7547be39
dcmartin <github@dcmartin.com>
2020-08-06 09:00:38 -0700

    lgtm

--- services/base-alpine/rootfs/usr/bin/hzn-tools.sh
+++ services/base-alpine/rootfs/usr/bin/hzn-tools.sh
@@ -7,7 +7,7 @@
 # hzn::pattern() - find the pattern with the given name; searches HZN_ORGANIZATION only
 hzn::pattern()
 {
-  bashio::log.trace "${FUNCNAME[0]} ${*}"
+  hzn::log.trace "${FUNCNAME[0]} ${*}"
 
   local pattern=${1:-}
   local json
@@ -16,16 +16,16 @@ hzn::pattern()
     local ALL=$(curl -sL -u "${HZN_ORGANIZATION}/${HZN_USER_ID:-iamapikey}:${HZN_EXCHANGE_APIKEY}" "${HZN_EXCHANGE_URL}orgs/${HZN_ORGANIZATION}/patterns")
 
     if [ ! -z "${ALL}" ]; then
-      bashio::log.trace "searching for pattern ${pattern} in all patterns ${ALL}"
+      hzn::log.trace "searching for pattern ${pattern} in all patterns ${ALL}"
       json=$(echo "${ALL}" | jq '.patterns|to_entries[]|select(.key=="'${pattern}'")')
     fi
   fi
   if [ ! -z "${pattern}" ]; then 
     if [ -z "${json:-}" ]; then 
-      bashio::log.warning "pattern was not found: ${pattern}"
+      hzn::log.warning "pattern was not found: ${pattern}"
     fi
   else
-    bashio::log.warning "pattern not specified"
+    hzn::log.warning "pattern not specified"
   fi
   echo ${json:-null}
 }
@@ -33,12 +33,12 @@ hzn::pattern()
 # initialize horizon
 hzn::init()
 {
-  bashio::log.trace "${FUNCNAME[0]}"
+  hzn::log.trace "${FUNCNAME[0]}"
 
   local config='{"timestamp":"'$(date -u +%FT%TZ)'","date":'$(date +%s)',"hzn":{"agreementid":"'${HZN_AGREEMENTID:-}'","arch":"'${HZN_ARCH:-}'","cpus":'${HZN_CPUS:-0}',"device_id":"'${HZN_DEVICE_ID:-}'","exchange_url":"'${HZN_EXCHANGE_URL:-}'","host_ips":['$(echo "${HZN_HOST_IPS:-}" | sed 's/,/","/g' | sed 's/\(.*\)/"\1"/')'],"organization":"'${HZN_ORGANIZATION:-}'","ram":'${HZN_RAM:-0}',"pattern":'$(hzn::pattern "${HZN_PATTERN:-}")'}}'
   local file=$(hzn::config.file)
 
-  bashio::log.debug "${FUNCNAME[0]}: writing horizon configuration; file: ${file}; config: ${config}"
+  hzn::log.debug "${FUNCNAME[0]}: writing horizon configuration; file: ${file}; config: ${config}"
 
   echo "${config}" > ${file} && cat ${file} || echo 'null'
 }
@@ -46,7 +46,7 @@ hzn::init()
 # get horizon configuration
 hzn::config()
 {
-  bashio::log.trace "${FUNCNAME[0]}"
+  hzn::log.trace "${FUNCNAME[0]}"
 
   local config
   local file=$(hzn::config.file)
@@ -54,26 +54,26 @@ hzn::config()
   if [ -s ${file} ]; then
     config=$(jq -c '.' ${file})
     if [ "${config:-null}" = 'null' ]; then
-      bashio::log.error "${FUNCNAME[@]}: invalid configuration: ${config:-}; file: ${file}"
+      hzn::log.error "${FUNCNAME[@]}: invalid configuration: ${config:-}; file: ${file}"
     else
-      bashio::log.debug "${FUNCNAME[0]}: valid configuration: ${config:-}; file: ${file}"
+      hzn::log.debug "${FUNCNAME[0]}: valid configuration: ${config:-}; file: ${file}"
     fi
   else
-    bashio::log.error "${FUNCNAME[@]}: zero-length configuration file: ${file}"
+    hzn::log.error "${FUNCNAME[@]}: zero-length configuration file: ${file}"
   fi
   echo "${config:-null}"
 }
 
 hzn::config.file()
 {
-  bashio::log.trace "${FUNCNAME[0]} ${*}"
+  hzn::log.trace "${FUNCNAME[0]} ${*}"
 
   echo "/var/run/horizon.json"
 }
 
 function hzn::log.level()
 {
-  bashio::log.trace "${FUNCNAME[0]} ${*}"
+  hzn::log.trace "${FUNCNAME[0]} ${*}"
 
   local log_level=$(bashio::string.lower "${1:-}")
   local level=${__BASHIO_LOG_LEVEL:-0}
@@ -82,18 +82,101 @@ function hzn::log.level()
     # Find the matching log level
     case "${log_level}" in
       all|trace|debug|info|notice|warning|error|fatal|critical|off)
-        bashio::log.level "${log_level}"
+        hzn::log.level "${log_level}"
         log_level="${__BASHIO_LOG_LEVELS[${__BASHIO_LOG_LEVEL:-0}]}"
-        bashio::log.debug "${FUNCNAME[0]}: updated log level; level: ${log_level:-null}"
+        hzn::log.debug "${FUNCNAME[0]}: updated log level; level: ${log_level:-null}"
         ;;
       *)
-        bashio::log.fatal "Unknown log_level: ${log_level}"
+        hzn::log.fatal "Unknown log_level: ${log_level}"
         log_level=""
         ;;
     esac
   else
     log_level="${__BASHIO_LOG_LEVELS[${__BASHIO_LOG_LEVEL:-0}]}"
-    bashio::log.debug "${FUNCNAME[0]}: reporting log level; level: ${log_level}"
+    hzn::log.debug "${FUNCNAME[0]}: reporting log level; level: ${log_level}"
   fi
   echo "${log_level}"
+}
+
+function hzn::log.red()
+{
+  bashio::log.trace "${FUNCNAME[0]} ${*}"
+  bashio::log.red ${*}
+}
+
+function hzn::log.green()
+{
+  bashio::log.trace "${FUNCNAME[0]} ${*}"
+  bashio::log.green ${*}
+}
+
+function hzn::log.yellow()
+{
+  bashio::log.trace "${FUNCNAME[0]} ${*}"
+  bashio::log.yellow ${*}
+}
+
+function hzn::log.blue()
+{
+  bashio::log.trace "${FUNCNAME[0]} ${*}"
+  bashio::log.blue ${*}
+}
+
+function hzn::log.magenta()
+{
+  bashio::log.trace "${FUNCNAME[0]} ${*}"
+  bashio::log.magenta ${*}
+}
+
+function hzn::log.cyan()
+{
+  bashio::log.trace "${FUNCNAME[0]} ${*}"
+  bashio::log.cyan ${*}
+}
+
+function hzn::log()
+{
+  bashio::log.trace "${FUNCNAME[0]} ${*}"
+  bashio::log ${*}
+}
+
+function hzn::log.trace()
+{
+  bashio::log.trace "${FUNCNAME[0]} ${*}"
+  bashio::log.trace ${*}
+}
+function hzn::log.debug()
+{ 
+  bashio::log.trace "${FUNCNAME[0]} ${*}"
+  bashio::log.debug ${*}
+}
+
+function hzn::log.info()
+{ 
+  bashio::log.trace "${FUNCNAME[0]} ${*}"
+  bashio::log.info ${*}
+}
+
+function hzn::log.notice()
+{ 
+  bashio::log.trace "${FUNCNAME[0]} ${*}"
+  bashio::log.notice ${*}
+}
+
+function hzn::log.warning()
+{ 
+  bashio::log.trace "${FUNCNAME[0]} ${*}"
+  bashio::log.warning ${*}
+}
+
+function hzn::log.error()
+{ 
+  bashio::log.trace "${FUNCNAME[0]} ${*}"
+  bashio::log.error ${*}
+}
+
+function hzn::log.fatal()
+{ 
+  bashio::log.trace "${FUNCNAME[0]} ${*}"
+  bashio::log.fatal ${*}
 }
--- services/base-alpine/rootfs/usr/bin/run.sh
+++ services/base-alpine/rootfs/usr/bin/run.sh
@@ -1,55 +1,49 @@
 #!/usr/bin/with-contenv bashio
 
+## hzn-tools.sh
+source /usr/bin/hzn-tools.sh
+
 ###
 ### run.sh
 ###
 
-bashio::log.notice $(date) "$0 $*"
-
-###
-### start
-###
-
-## hzn-tools.sh
-source /usr/bin/hzn-tools.sh
-
-run()
+run::main()
 {
-  bashio::log.trace "${FUNCNAME[0]} ${*}"
+  hzn::log.trace "${FUNCNAME[0]} ${*}"
 
   local init=$(hzn::init)
 
   # initialize horizon
   if [ -z "${init:-}" ]; then
-    bashio::log.error "${FUNCNAME[0]}: horizon initialization failed"
+    hzn::log.error "${FUNCNAME[0]}: horizon initialization failed"
   else
     local config=$(hzn::config)
 
     if [ -z "${config:-}" ]; then
-      bashio::log.error "${FUNCNAME[0]}: no horizon configuration"
+      hzn::log.error "${FUNCNAME[0]}: no horizon configuration"
     else
-      bashio::log.debug "${FUNCNAME[0]}: init: ${init:-}; config: ${config:-}"
+      hzn::log.debug "${FUNCNAME[0]}: init: ${init:-}; config: ${config:-}"
 
       # label
       if [ ! -z "${SERVICE_LABEL:-}" ]; then
         local CMD=$(command -v "${SERVICE_LABEL:-}.sh")
   
         if [ ! -z "${CMD}" ]; then
-          bashio::log.info "${FUNCNAME[0]}: starting command: ${CMD}"
+          hzn::log.info "${FUNCNAME[0]}: starting command: ${CMD}"
           ${CMD} &
         fi
 
         # port
         if [ -z "${SERVICE_PORT:-}" ]; then
           SERVICE_PORT=80
-          bashio::log.warning "${FUNCNAME[0]}: service port not specified; using localhost port ${SERVICE_PORT}"
+          hzn::log.warning "${FUNCNAME[0]}: service port not specified; using localhost port ${SERVICE_PORT}"
         fi
 
         # start listening
-        bashio::log.info "${FUNCNAME[0]}: service listening on http://localhost:${SERVICE_PORT}"
+        hzn::log.info "${FUNCNAME[0]}: service listening on http://localhost:${SERVICE_PORT}"
         socat TCP4-LISTEN:${SERVICE_PORT},fork EXEC:service.sh
       else
-        bashio::log.warning "${FUNCNAME[0]}: executable ${SERVICE_LABEL:-}.sh not found"
+        hzn::log.warning "${FUNCNAME[0]}: executable ${SERVICE_LABEL:-}.sh not found"
       fi
     fi
   fi
@@ -62,6 +56,6 @@ run()
 # TMPDIR
 if [ -d '/tmpfs' ]; then export TMPDIR=${TMPDIR:-/tmpfs}; else export TMPDIR=${TMPDIR:-/tmp}; fi
 
-bashio::log.notice "${0} ${*}; TMPDIR: ${TMPDIR:-null}; LOG_LEVEL: ${LOG_LEVEL:-null}"
+hzn::log.notice "${0} ${*}; TMPDIR: ${TMPDIR:-null}; LOG_LEVEL: ${LOG_LEVEL:-null}"
 
-run ${*}
+run::main ${*}
