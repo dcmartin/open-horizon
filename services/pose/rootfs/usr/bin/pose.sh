4363dbfdf61dfb007cdf9d79226b602c020c3e56
dcmartin <github@dcmartin.com>
2021-02-22 12:30:13 -0800

    wip

--- services/pose/rootfs/usr/bin/pose.sh
+++ services/pose/rootfs/usr/bin/pose.sh
@@ -1,76 +1,126 @@
-#!/bin/bash
+#!/usr/bin/with-contenv /usr/bin/bashio
 
-# TMPDIR
-if [ -d '/tmpfs' ]; then TMPDIR='/tmpfs'; else TMPDIR='/tmp'; fi
-
-###
-### FUNCTIONS
-###
+# ==============================================================================
+set -o nounset  # Exit script on use of an undefined variable
+set -o pipefail # Return exit status of the last command in the pipe that failed
+set -o errexit  # Exit script when a command exits with non-zero status
+set -o errtrace # Exit on error inside any functions or sub-shells
 
 source /usr/bin/service-tools.sh
 source /usr/bin/pose-tools.sh
+
+function pose::main()
+{
+  hzn::log.trace "${FUNCNAME[0]} ${*}"
+
+  local VALUE
+  local TIMEZONE
+  local PROJECT
+  local WORKSPACE
+  local PORT
+  local HOST
+  local PROTOCOL
+  local USERNAME
+  local PASSWORD
+  local INIT
+  local PID
+
+  # TIMEZONE
+  VALUE=$(bashio::config "timezone")
+  if [ -z "${VALUE}" ] || [ "${VALUE}" == "null" ]; then VALUE="GMT"; fi
+  hzn::log.info "Setting timezone: ${VALUE}" >&2
+  TIMEZONE=${VALUE}
+  cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+
+  # WORKSPACE
+  VALUE=$(bashio::config "workspace")
+  if [ -z "${VALUE}" ] || [ "${VALUE}" == "null" ]; then VALUE=${LABELSTUDIO_WORKSPACE:-'/data/pose'}; fi
+  hzn::log.info "Setting project: ${VALUE}" >&2
+  WORKSPACE=${VALUE}
+
+  # PROJECT
+  VALUE=$(bashio::config "project")
+  if [ -z "${VALUE}" ] || [ "${VALUE}" == "null" ]; then VALUE=${LABELSTUDIO_PROJECT:-'MyProject'}; fi
+  hzn::log.info "Setting project: ${VALUE}" >&2
+  PROJECT=${VALUE}
+
+  # HOST
+  VALUE=$(bashio::config "host")
+  if [ -z "${VALUE}" ] || [ "${VALUE}" == "null" ]; then VALUE=${LABELSTUDIO_HOST:-'0.0.0.0'}; fi
+  hzn::log.info "Setting host: ${VALUE}" >&2
+  HOST=${VALUE}
+
+  # PORT
+  VALUE=$(bashio::config "port")
+  if [ -z "${VALUE}" ] || [ "${VALUE}" == "null" ]; then VALUE=${LABELSTUDIO_PORT:-'7998'}; fi
+  hzn::log.info "Setting port: ${VALUE}" >&2
+  PORT=${VALUE}
+
+  # PROTOCOL
+  VALUE=$(bashio::config "protocol")
+  if [ -z "${VALUE}" ] || [ "${VALUE}" == "null" ]; then VALUE=${LABELSTUDIO_PROTOCOL:-'http://'}; fi
+  hzn::log.info "Setting protocol: ${VALUE}" >&2
+  PROTOCOL=${VALUE}
+
+  # USERNAME
+  VALUE=$(bashio::config "username")
+  if [ -z "${VALUE}" ] || [ "${VALUE}" == "null" ]; then VALUE=${LABELSTUDIO_USERNAME:-'username'}; fi
+  hzn::log.info "Setting username: ${VALUE}" >&2
+  USERNAME=${VALUE}
+
+  # PASSWORD
+  VALUE=$(bashio::config "password")
+  if [ -z "${VALUE}" ] || [ "${VALUE}" == "null" ]; then VALUE=${LABELSTUDIO_PASSWORD:-'password'}; fi
+  hzn::log.info "Setting password: ${VALUE}" >&2
+  PASSWORD=${VALUE}
+
+  # INIT
+  VALUE=$(bashio::config "init")
+  if [ -z "${VALUE}" ] || [ "${VALUE}" == "null" ]; then VALUE=${LABELSTUDIO_INIT:-'--init'}; fi
+  hzn::log.info "Setting init: ${VALUE}" >&2
+  INIT=${VALUE}
+
+  # START 
+  local config='{"timestamp":"'$(date -u +%FT%TZ)'","log_level":"'$(bashio::config "log_level")'","period":'${SERVICE_PERIOD:-30}',"label":"'${SERVICE_LABEL:-}'","timezone":"'${TIMEZONE}'","hostname":"'$(hostname)'","arch":"'$(arch)'","date":'$(/bin/date +%s)',"'${SERVICE_LABEL:-}'":{"workspace":"'${WORKSPACE}'","project":"'${PROJECT}'","host":"'${HOST}'","port":'${PORT}',"protocol":"'${PROTOCOL}'","username":"'${USERNAME}'","password":"'${PASSWORD}'","init":"'${INIT}'"}}'
+
+  local output=$(mktemp)
+
+  hzn::log.notice "${FUNCNAME[0]}: initializing service: ${SERVICE_LABEL:-}" $(echo "${config}" | jq -c '.' || echo "INVALID: ${config}")
+  hzn::service.init "${config}"
+
+  # start pose
+  PID=$(pose::start "${config}")
+
+  # loop while node is alive
+  while [ true ]; do
+
+    # update pose status
+    pose::service.update ${output}
+
+    # update horizon
+    if [ -s "${output}" ]; then
+      hzn::log.info "${FUNCNAME[0]}: updating service: ${SERVICE_LABEL:-null}"
+    else
+      hzn::log.warn "${FUNCNAME[0]}: no service update output"
+      echo '{"service":"'${SERVICE_LABEL:-}'","error":"no output"}' > ${output}
+    fi
+
+    hzn::service.update ${output}
+
+    # sleep
+    hzn::log.debug "${FUNCNAME[0]}: sleeping for ${SERVICE_PERIOD:-30} seconds..."
+    sleep ${SERVICE_PERIOD:-30}
+  done
+  rm -f ${output} ${tmp} ${err}
+}
 
 ###
 ### MAIN
 ###
 
-## initialize horizon
-hzn_init
+hzn::log.notice "${0} ${*}"
 
-## initialize servive
-CONFIG=$(echo $(pose_init) | jq '.resolution="'${WEBCAM_RESOLUTION}'"|.device="'${WEBCAM_DEVICE}'"')
+# TMPDIR
+if [ -d '/tmpfs' ]; then export TMPDIR=${TMPDIR:-/tmpfs}; else export TMPDIR=${TMPDIR:-/tmp}; fi
 
-service_init "${CONFIG}"
-
-## initialize
-OUTPUT_FILE="${TMPDIR}/${0##*/}.${SERVICE_LABEL}.$$.json"
-echo '{"timestamp":"'$(date -u +%FT%TZ)'","date":'$(date +%s)'}' > "${OUTPUT_FILE}"
-
-## configure POSE
-pose_config ${POSE_CONFIG:-}
-
-# start in pose
-cd ${OPENPOSE}
-
-hzn::log.notice "processing images from /dev/video0 every ${POSE_PERIOD} seconds"
-
-if [ -z "${WEBCAM_DEVICE}" ]; then WEBCAM_DEVICE="/dev/video0"; fi
-if [ -z "${WEBCAM_RESOLUTION}" ]; then WEBCAM_RESOLUTION="320x240"; fi
-
-while true; do
-  # when we start
-  DATE=$(date +%s)
-
-  # path to image payload
-  JPEG_FILE=$(mktemp -t "${0##*/}-XXXXXX")
-  # capture image payload from /dev/video0
-  # fswebcam --resolution "${WEBCAM_RESOLUTION}" --device "${WEBCAM_DEVICE}" --no-banner "${JPEG_FILE}" &> /dev/null
-
-  hzn::log.debug "Attempting to capture image"
-  fswebcam --device "${WEBCAM_DEVICE}" --no-banner "${JPEG_FILE}" &> /dev/null
-
-  # process image payload into JSON
-  if [ -z "${ITERATION:-}" ]; then ITERATION=0; else ITERATION=$((ITERATION+1)); fi
-  POSE_OUTPUT_FILE=$(pose_process "${JPEG_FILE}" "${ITERATION}")
-
-  # remove
-  rm -f ${JPEG_FILE}
-
-  if [ -s "${POSE_OUTPUT_FILE}" ]; then
-    # add two files
-    jq '.timestamp="'$(date -u +%FT%TZ)'"|.date='$(date +%s) "${POSE_OUTPUT_FILE}" > "${OUTPUT_FILE}"
-    # update
-  else
-    hzn::log.error "No output"
-    echo '{"timestamp":"'$(date -u +%FT%TZ)'","date":'$(date +%s)'}' > "${OUTPUT_FILE}"
-  fi
-  # update
-  service_update "${OUTPUT_FILE}"
-
-  # wait for ..
-  SECONDS=$((POSE_PERIOD - $(($(date +%s) - DATE))))
-  if [ ${SECONDS} -gt 0 ]; then
-    hzn::log.debug "sleep ${SECONDS}"
-    sleep ${SECONDS}
-  fi
-done
+pose::main ${*}
